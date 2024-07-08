746c0b258a5aaebcbd7ae39c8031b65996e0dfdd
unknown <19nrjsharma1995@gmail.com>
2024-07-08 15:33:13 +0530

    header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,27 @@
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = { firstName: "Neeraj" }
     return (
-        <div>
-            Home
-        </div>
+        <section className='home'>
+            <div className='home-content'>
+                <header className='home-header'>
+                    <HeaderBox
+                        type='greeting'
+                        title='Welcome'
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your account"
+                    />
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalCurrentBalance={1250.21}
+                        totalBanks={1}
+                    />
+                </header>
+            </div>
+        </section>
     )
 }
 
---
+++ components/ui/DoughnutChart.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { Doughnut } from 'react-chartjs-2';
+import { Chart, ArcElement, Tooltip, Legend } from 'chart.js'
+
+Chart.register(ArcElement);
+const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+    const data = {
+        datasets: [
+            {
+                label: 'Banks',
+                data: [1250, 2500, 453],
+                backgroundColor: ['#0747b6', '#2265d8', '#2f91fa']
+            }
+        ],
+        labels: ['Bank1', 'Bank2', 'Bank3']
+    }
+    return (
+        <Doughnut data={data}
+            options={{
+                cutout: '60%',
+                plugins: {
+                    legend: {
+                        display: false
+                    }
+                }
+            }} />
+    )
+}
+
+export default DoughnutChart
