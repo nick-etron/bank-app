c523567d20e5833ef2e94d1fc5f4096cf035344f
Neeraj Sharma <nicksharma@724gmail.com>
2024-07-09 16:23:35 +0530

    fix

--- components/ui/BankCard.tsx
+++ components/ui/BankCard.tsx
@@ -1,4 +1,5 @@
 import { formatAmount } from '@/lib/utils'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -18,11 +19,34 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
                     <article className='flex flex-col gap-2'>
                         <div className='flex justify-between'>
                             <h1 className='text-12 font-semibold text-white'>{userName}</h1>
-                            <h2 className='text-12 font-semibold text-white'>{userName}</h2>
+                            <h2 className='text-12 font-semibold text-white'>** / **</h2>
                         </div>
+                        <p className='text-14 font-semibold tracking-[1.1px] text-white'>
+                            ◍◍◍◍ ◍◍◍◍ ◍◍◍◍ <span className='text-16'>1234</span>
+                        </p>
                     </article>
                 </div>
+                <div className='bank-card_icon'>
+                    <Image src='/icons/Paypass.svg'
+                        width={20}
+                        height={20}
+                        alt='pay'
+                    ></Image>
+                    <Image src='/icons/mastercard.svg'
+                        width={45}
+                        height={32}
+                        alt='mastercard'
+                        className='ml-5'
+                    ></Image>
+                </div>
+                <Image src='/icons/Lines.png'
+                    width={316}
+                    height={190}
+                    alt='mastercard'
+                    className='absolute top-0 left-0'
+                ></Image>
             </Link>
+
         </div>
     )
 }
